033f3640e502c0fe0d7f18f2b2df4467333132a7
Lucas Safeno <lugnorio@gmail.com>
2024-02-07 13:18:18 -0300

    11 JavaScript - 2 JavaScript básico - Introdução array - #1095

---
+++ 11-Javascript/02-Javascript_básico/13-introducao_array/script.js
@@ -0,0 +1,17 @@
+// Arrays
+
+let ingredientes = [
+  'agua',
+  'farinha',
+  'ovo',
+  'corante',
+  'sal'
+];
+
+// Parada : 04:45min
+
+
+// length -> Quantidade de elementos array
+console.log(`Total de Ingrdientes : ${ingredientes.length}`);
+
+// Adicionar item no array
