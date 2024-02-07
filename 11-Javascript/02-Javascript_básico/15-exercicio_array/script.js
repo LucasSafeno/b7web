51e7a86a21835ae3f51fd877b5cd966429ec2831
Lucas Safeno <lugnorio@gmail.com>
2024-02-07 13:29:29 -0300

    11 JavaScript - 2 JavaScript básico - Exercicio array - #1097

---
+++ 11-Javascript/02-Javascript_básico/15-exercicio_array/script.js
@@ -0,0 +1,18 @@
+// 1. No array abaixo, qual o número que pega o Ferray?
+let carros = ['BMW', 'Ferrari', 'Mercedes'];
+let x = 1;
+console.log('1. ' + carros[x]);
+
+// 2. Troque a Ferrari por Audi
+console.log('2. Lista com Audi :');
+carros[1] = "Audi";
+console.log(carros);
+
+// 3. Adicione o Volvo a lista
+console.log('3. Lista com Volvo :');
+carros.push("Volvo");
+console.log(carros);
+
+// 4. Exiba quantos itens tem no array
+console.log('4. Itens no array : ');
+console.log(carros.length);
