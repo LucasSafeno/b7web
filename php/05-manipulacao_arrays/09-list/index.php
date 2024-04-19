4d7d09eeaf854abf69d1b5a2735cda9acf6682a9
Lucas Safeno <lugnorio@gmail.com>
2024-04-19 14:45:21 -0300

    B7WEB - 17 - PHP - 5 Manipulação Array -  - Descontruindo usando list

---
+++ php/05-manipulacao_arrays/09-list/index.php
@@ -0,0 +1,6 @@
+<?php
+$array = ['Lucas', 29, 'Café', 'Preto'];
+
+list($nome, $idade, $bebida, $cor) = $array;
+
+echo $nome . " tem " . $idade . " anos e gosta de tomar " . $bebida . " com a cor " . $cor;
