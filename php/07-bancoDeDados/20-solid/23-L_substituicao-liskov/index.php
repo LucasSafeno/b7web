f80b0c5db7d5a3861912b612f4ef1c59ca97bef1
Lucas Safeno <lugnorio@gmail.com>
2024-04-23 10:27:52 -0300

    B7WEB - 17 - PHP 7 - Banco de dados - 23 - SOLID_ L - Princípio da Substituição de Liskov #F2268

---
+++ php/07-bancoDeDados/20-solid/23-L_substituicao-liskov/index.php
@@ -0,0 +1,28 @@
+<?php
+class A
+{
+  public function getNome()
+  {
+    return 'Meu nome é A';
+  }
+}
+
+class B extends A
+{
+  public function getNome()
+  {
+    return 'Meu nome é B';
+  }
+}
+
+function imprimeNome(A $obj)
+{
+  return $obj->getNome();
+}
+
+$objeto1 = new A();
+$objeto2 = new B();
+
+echo imprimeNome($objeto1);
+echo "<br>";
+echo imprimeNome($objeto2);
