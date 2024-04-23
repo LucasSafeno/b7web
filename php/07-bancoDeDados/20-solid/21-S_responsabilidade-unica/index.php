8adf18646637f0b3d05e9867ca74ce6a99f027a2
Lucas Safeno <lugnorio@gmail.com>
2024-04-23 10:01:18 -0300

    B7WEB - 17 - PHP 7 - Banco de dados - 21 - SOLID_ S - Princípio da Responsabilidade Única - #F2266

---
+++ php/07-bancoDeDados/20-solid/21-S_responsabilidade-unica/index.php
@@ -0,0 +1,47 @@
+<?php
+// class Usuario
+// {
+//   public function setNome()
+//   {
+//   }
+//   public function getNome()
+//   {
+//   }
+
+//   public function add()
+//   {
+//   }
+//   public function update()
+//   {
+//   }
+//   public function delete()
+//   {
+//   }
+
+//   // Email
+//   // Senha
+// }
+
+class Usuario
+{
+  public function setNome()
+  {
+  }
+  public function getNome()
+  {
+  }
+}
+
+class UsuarioDb
+{
+  public function add(Usuario $u)
+  {
+  }
+
+  public function update(Usuario $u)
+  {
+  }
+  public function delete($u)
+  {
+  }
+} // UsuarioDB
