31faaf7f28fc598c4c73c94561ec5eba35335156
Lucas Safeno <lugnorio@gmail.com>
2024-04-22 23:36:31 -0300

    B7WEB - 17 - PHP 7 - Banco de dados - 16 - Criando um DAO na prática (2_5)

---
+++ php/07-bancoDeDados/15-dao/config.php
@@ -0,0 +1,7 @@
+<?php
+$db_name      = "test";
+$db_user      = "root";
+$db_password  = "root";
+$db_host      = "localhost";
+
+$pdo = new PDO("mysql:dbname=" . $db_name . ";host=" . $db_host, $db_user, $db_password);
--- php/07-bancoDeDados/15-dao/models/Usuario.php
+++ php/07-bancoDeDados/15-dao/models/Usuario.php
@@ -29,7 +29,7 @@ class Usuario
     return $this->email;
   }
 
-  public function setEmal($e)
+  public function setEmail($e)
   {
     $this->email = strtolower(trim($e));
   }
