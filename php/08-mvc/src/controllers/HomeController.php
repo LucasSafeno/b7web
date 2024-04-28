cc409302b72f6df50560b080ac25893a731cfef6
Lucas Safeno <lugnorio@gmail.com>
2024-04-28 14:14:49 -0300

    B7WEB - 17 PHP - 8 - Padrão MVC(Model, View, Controller) - 1 - Introdução ao MV

--- php/08-mvc/src/controllers/HomeController.php
+++ php/08-mvc/src/controllers/HomeController.php
@@ -1,20 +1,30 @@
 <?php
+
 namespace src\controllers;
 
 use \core\Controller;
+use \src\models\Test;
 
-class HomeController extends Controller {
+class HomeController extends Controller
+{
 
-    public function index() {
-        $this->render('home', ['nome' => 'Bonieky']);
+    public function fotos()
+    {
+        $test = new Test();
     }
 
-    public function sobre() {
+    public function index()
+    {
+        $this->render('home', ['nome' => 'Lucas']);
+    }
+
+    public function sobre()
+    {
         $this->render('sobre');
     }
 
-    public function sobreP($args) {
+    public function sobreP($args)
+    {
         print_r($args);
     }
-
 }
--- php/08-mvc/src/routes.php
+++ php/08-mvc/src/routes.php
@@ -1,7 +1,10 @@
 <?php
+
 use core\Router;
 
 $router = new Router();
+
+$router->get('/fotos', 'HomeController@fotos');
 
 $router->get('/', 'HomeController@index');
 $router->get('/sobre/{nome}', 'HomeController@sobreP');
